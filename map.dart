a21fa9d582cc5b01cb3fe88caefcfed7dc078e6a
Muhammad Raza Naqvi <147847670+mastonaqvi@users.noreply.github.com>
2024-09-14 18:25:34 +0500

    Create map.dart

---
+++ map.dart
@@ -0,0 +1,58 @@
+// void main(){
+
+//   var map_name = {
+//     'Name': 'Raza',
+//     'Age': 20,
+//     'Cgpa': 3.14,
+//     'Health': true
+//   };
+//     //print(map_name);
+
+//     map_name['key1'] = 'Muhammad';
+//     //print(map_name);
+
+//     //print(map_name['key2']);
+
+//     var mapName = Map();
+
+//     mapName ['Key1'] = "Ali";
+//     mapName ['Key2'] = "Hassan";
+//     mapName ['Key3'] = "Hussain";
+
+//     print(mapName.isEmpty);
+//     print(mapName.isNotEmpty);
+//     print(mapName.length);
+//     print(mapName.keys);
+//     print(mapName.values);
+//     print(mapName.containsKey('Name'));
+//     print(mapName.containsValue(false));
+//     print(mapName.remove('Health'));
+//     print(mapName);
+// }
+
+
+
+
+// void main(){
+//   final names = [
+//     "Ali",
+//     "Hassan",
+//     "Hussain",
+//   ];
+//   names.add('Zain');
+//   names = ["Muhammad", "Raza", "Naqvi"];
+// }
+
+void main(){
+  var a = 80;
+  var b = 100;
+  if(a>=100 || b>=150){
+    print('True');
+  }else{
+    print('false');
+  }
+  for (int i=0; i<10; i++){
+    print('ALi');
+  }
+
+}
